5d560d8bc6f8f92ffdaaec0ebaf9c301ee64d0fb
pablomdd <pablo.mdd.007@gmail.com>
2020-03-21 22:22:48 -0600

    Add Day 4: classes

---
+++ D4_count_objects.js
@@ -0,0 +1,29 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let counter = 0;
+
+    for(let i of objects){
+        if(i.x === i.y) counter++;
+    }
+
+    return counter;
+}
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
